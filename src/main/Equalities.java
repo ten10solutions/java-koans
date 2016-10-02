b190525fe9c077e4a61fa0970b89409d39c22f80
Jack Forman <jack.forman@thetestpeople.com>
2016-10-02 16:56:42 +0100

    finished off equalities unit tests - renamed test class

--- src/main/Equalities.java
+++ src/main/Equalities.java
@@ -28,54 +28,58 @@ public class Equalities {
         assert(
 
 
-                true == true
+                BLANK == true
 
         );
         return true;
     }
 
     //  2. We can determine difference as well as equality
-    @Test
-    public void testingReality(){
+
+    public Boolean testingReality(){
         assert(
 
                 BLANK != true
         );
+        return true;
     }
     //  3. We can test the equality of nothing
-    @Test
-    public void equalityOfNothing(){
+
+    public Boolean equalityOfNothing(){
         assert(
-                BLANK == null
+                BLANK != null
         );
+        return true;
 
     }
     //  4. To understand reality, we must compare our expectations against reality
-    @Test
-    public void exectationsVsReality(){
+
+    public Boolean exectationsVsReality(){
         assert(
                 BLANKint  == (1 + 2)
         );
-
+        return true;
     }
 
     //  5. You can test equality of many things
-    @Test
-    public void equalityOfManyThings(){
+
+    public Boolean equalityOfManyThings(){
         assert(
                 (3 + 4) ==  BLANKint  && BLANKint == (5 + 2)
         );
-
+        return true;
     }
 
     // 6. The question you ask determines the outcome
-    @Test
-    public void differentButSimilar(){
+
+    public Boolean differentButSimilar(){
         int[] a = {1, 2, 3};
         int[] b = {1, 2, 3};
-        assert (
-                a == b
+        assert(
+                a==b
+
         );
+        return true;
     }
 
 }
