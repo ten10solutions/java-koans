b4ff7085fcbff2b9884381c72399db26836c4bf2
jack.forman <forman.jack@gmail.com>
2016-10-09 18:54:02 +0100

    finished Arrays and Lists

--- src/main/ArraysAndLists.java
+++ src/main/ArraysAndLists.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
@@ -24,12 +25,31 @@ public class ArraysAndLists {
         }
 
 //  2. We can create an Array by its size
-    public void newArrayBySize(){
+    public int[] newArrayBySize(){
         int[] myNewArray = new int[10];
-        System.out.print(myNewArray);
+        myNewArray[0] = 1;
+        myNewArray[1] = 2;
+        return myNewArray;
     }
 
-//  2. We can't add to an Array - introducing the List
+//  3. We can do something with each element of a list
+    public int[] iterateThroughArray(){
+        int[] myNewArray = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i<=9; i++){
+
+            System.out.print(myNewArray[i]);
+        }
+        return myNewArray;
+    }
+
+//  4. We can sort the elements in an array
+    public int[] sortArray(){
+        int[] myUnsortedArray = {1,6,5,9,2,4,3,7,10};
+        Arrays.sort(myUnsortedArray);
+        return myUnsortedArray;
+    }
+
+//  5. We can't add to an Array - introducing the List
 
     public List<Integer> addToAList(){
         List<Integer> myNewList = new ArrayList<>();
@@ -39,13 +59,48 @@ public class ArraysAndLists {
         return myNewList;
     }
 
-//  3. We can access the values of a List
+//  6. We can access the values of a List
     public int accessListValues(){
         List<Integer> mySimpleList = Arrays.asList(1, 3, 5, 7, 9);
         int theFourthValue = mySimpleList.get(2);
         System.out.print(theFourthValue);
         return theFourthValue;
     }
+
+//  7. We can remove a value from a List
+    public ArrayList<Integer> removeFromList() {
+        ArrayList<Integer> myNewList = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9));
+        myNewList.remove(1);
+        System.out.print("myNewList = " + myNewList);
+        return myNewList;
+    }
+
+//  8. We can iterate through a list easily
+    public ArrayList<Integer> iterateThroughArrayList(){
+        ArrayList<Integer> myNewList = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9));
+
+        for (int i = 0; i<=4; i++){
+            int value = myNewList.get(i);
+
+        }
+        System.out.print(myNewList);
+        return myNewList;
+
+    }
+
+//  9. Or we can use an Iterator
+    public ArrayList<Integer> useAnIterator(){
+        ArrayList<Integer> myNewList = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9));
+        Iterator newIterator = myNewList.iterator();
+        while(newIterator.hasNext()){
+            System.out.print(newIterator.next());
+        }
+        return myNewList;
+    }
+
+
+
+
 
 
 
--- src/test/ArraysAndListsCheckYourWork.java
+++ src/test/ArraysAndListsCheckYourWork.java
@@ -1,6 +1,7 @@
 package test;
 
 import junit.framework.Assert;
+import junit.framework.AssertionFailedError;
 import main.ArraysAndLists;
 import org.junit.Test;
 
@@ -9,6 +10,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Created by jack.forman on 05/10/2016.
@@ -25,8 +29,17 @@ public class ArraysAndListsCheckYourWork {
     @Test
     public void newArrayBySizeTest(){
         ArraysAndLists arrays = new ArraysAndLists();
-        arrays.newArrayBySize();
-        //assertEquals("That doesnt look right? you think the third number == " + answer,3,answer);
+        int[] answer = {1,2,3,4,5,6,7,8,9,10};
+        int[] result = arrays.newArrayBySize();
+        try {
+            assertTrue(Arrays.equals(answer, result));
+        } catch (AssertionError e){
+            System.out.println("Try adding some more values to your array, so it matches this: ");
+            for(int val:answer){
+                System.out.print(val+",");
+            }
+            fail();
+        }
     }
 
 
@@ -43,6 +56,65 @@ public class ArraysAndListsCheckYourWork {
         ArraysAndLists arrays = new ArraysAndLists();
         int result = arrays.accessListValues();
         assertEquals("So you think the fourth value is " + result,7,result);
+    }
+
+    @Test
+    public void iterateThroughArrayTest(){
+        ArraysAndLists arrays = new ArraysAndLists();
+        int[] answer = {2,3,4,5,6,7,8,9,10,11};
+        int[] result = arrays.iterateThroughArray();
+        try {
+            assertTrue(Arrays.equals(answer, result));
+        } catch (AssertionError e){
+            System.out.println("\nTry adding in some code, to produce the following values - ");
+            for(int val:answer){
+                System.out.print(val);
+            }
+            fail();
+        }
+    }
+
+    @Test
+    public void sortArrayTest(){
+        ArraysAndLists arrays = new ArraysAndLists();
+        int[] answer = {1,2,3,4,5,6,7,8,9,10};
+        int[] result = arrays.sortArray();
+        try {
+            assertTrue(Arrays.equals(answer, result));
+            System.out.print("Yep!  You just needed to add an 8");
+        } catch (AssertionError e){
+            System.out.print("Are you sure there are enough values in your array?\n I'm looking for - ");
+            for(int val:answer){
+                System.out.print(val);
+            }
+            fail();
+        }
+    }
+
+    @Test
+    public void removeFromListTest() {
+        ArraysAndLists arrays = new ArraysAndLists();
+        ArrayList<Integer> answer = new ArrayList<>(Arrays.asList(1, 3, 7, 9));
+        ArrayList<Integer> result = arrays.removeFromList();
+        assertEquals("Try removing one value from the list so it matches the below",answer,result);
+    }
+
+    @Test
+    public void iterateThroughArrayListTest() {
+        ArraysAndLists arrays = new ArraysAndLists();
+        ArrayList<Integer> answer = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10));
+        ArrayList<Integer> result = arrays.iterateThroughArrayList();
+
+        assertEquals("I am using list.get() to get a value, maybe experiment with list.set() to change them " +
+                "to match the below", answer,result);
+    }
+
+    @Test
+    public void useAnIteratorTest() {
+        ArraysAndLists arrays = new ArraysAndLists();
+        ArrayList<Integer> answer = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9, 11));
+        ArrayList<Integer> result = arrays.useAnIterator();
+        assertEquals("Try altering the values so they match the below", answer,result);
     }
 
 }
